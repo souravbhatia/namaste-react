ea1e19df7c59f89b9cfbd5ed6dc10cd227d70d55
Sourav Kumar <saurav.kumar@applify.co>
2024-05-05 16:48:10 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,21 @@
+// const heading = React.createElement("h1", { id: "heading" }, "Hello world from react");
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);
+
+
+/** 
+ * <div id="parent">
+    <div id="child">
+        <h1>Hii</h1>
+        <h2>How are you?</h2>
+    </div>
+</div> 
+*/
+
+const parent = React.createElement("div", { id: "parent" }, React.createElement("div", { id: "child" }, [React.createElement("h1", {}, "Hello"), React.createElement("h2", {}, "How are you?")]))
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
